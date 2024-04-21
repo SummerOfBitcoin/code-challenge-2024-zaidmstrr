781db57cadac79ce0f2b7b98d2bf51f436e328ad
zaidmstrr <zaidbrock122@gmail.com>
2024-04-21 14:49:25 +0530

    Test Commit 5

--- test_serialisation.js
+++ test_serialisation.js
@@ -246,7 +246,7 @@ function serialization(transaction, isLastFile) {
         let hexAmount = amountBuffer.toString('hex');
         
         let witnessTransaction = "010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff2503233708184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100ffffffff02" 
-        +  hexAmount + "1976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9ed" +  witnessHashed + "0120000000000000000000000000000000000000000000000000000000000000000000000000"
+        +  hexAmount + "1976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9ed" +  wxidCommitment + "0120000000000000000000000000000000000000000000000000000000000000000000000000"
         // let witnessTransactionBuffer = Buffer.from(witnessTransaction, 'hex');
         // // hash the buffer
         // let hashedWitnessTransaction = doubleSHA256(witnessTransactionBuffer);
@@ -257,7 +257,7 @@ function serialization(transaction, isLastFile) {
         // let reversedHashedWitnessHex = reversedHashedWitnessTransaction.toString('hex');
 
         let coinbaseinHex = "010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff2503233708184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100ffffffff02" 
-        +  hexAmount + "1976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9ed" +  witnessHashed + "00000000"
+        +  hexAmount + "1976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9ed" +  wxidCommitment + "00000000"
         let coinbaseHash = doubleSHA256(Buffer.from(coinbaseinHex, 'hex'));
         // Reverse the hash to get the txid
         let coinbasetxid = coinbaseHash.reverse().toString('hex');
