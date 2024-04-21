a2a2f1bbcfda00ec003dd4966237c6acec40430d
zaidmstrr <zaidbrock122@gmail.com>
2024-04-21 14:51:11 +0530

    Test Commit 5

--- test_serialisation.js
+++ test_serialisation.js
@@ -247,26 +247,26 @@ function serialization(transaction, isLastFile) {
         
         let witnessTransaction = "010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff2503233708184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100ffffffff02" 
         +  hexAmount + "1976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9ed" +  wxidCommitment + "0120000000000000000000000000000000000000000000000000000000000000000000000000"
-        // let witnessTransactionBuffer = Buffer.from(witnessTransaction, 'hex');
-        // // hash the buffer
-        // let hashedWitnessTransaction = doubleSHA256(witnessTransactionBuffer);
-        // // reverse the bytes 
-        // let reversedHashedWitnessTransaction = hashedWitnessTransaction.reverse();
+        let witnessTransactionBuffer = Buffer.from(witnessTransaction, 'hex');
+        // hash the buffer
+        let hashedWitnessTransaction = doubleSHA256(witnessTransactionBuffer);
+        // reverse the bytes 
+        let reversedHashedWitnessTransaction = hashedWitnessTransaction.reverse();
 
         // Convert the reversed hash back to a hexadecimal string
-        // let reversedHashedWitnessHex = reversedHashedWitnessTransaction.toString('hex');
+        let reversedHashedWitnessHex = reversedHashedWitnessTransaction.toString('hex');
 
-        let coinbaseinHex = "010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff2503233708184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100ffffffff02" 
-        +  hexAmount + "1976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9ed" +  wxidCommitment + "00000000"
-        let coinbaseHash = doubleSHA256(Buffer.from(coinbaseinHex, 'hex'));
-        // Reverse the hash to get the txid
-        let coinbasetxid = coinbaseHash.reverse().toString('hex');
+        // let coinbaseinHex = "010000000001010000000000000000000000000000000000000000000000000000000000000000ffffffff2503233708184d696e656420627920416e74506f6f6c373946205b8160a4256c0000946e0100ffffffff02" 
+        // +  hexAmount + "1976a914edf10a7fac6b32e24daa5305c723f3de58db1bc888ac0000000000000000266a24aa21a9ed" +  wxidCommitment + "00000000"
+        // let coinbaseHash = doubleSHA256(Buffer.from(coinbaseinHex, 'hex'));
+        // // Reverse the hash to get the txid
+        // let coinbasetxid = coinbaseHash.reverse().toString('hex');
         // coinbse END
 
         console.log("number of valid transactions: " + numOfP2wpkhTrans);
         console.log("The total fee is: " + totalfee);
 
-        txids.unshift(coinbasetxid)
+        txids.unshift(reversedHashedWitnessHex)
         // Reversing each txid to match Ruby's example
         txids = txids.map(txid => txid.match(/../g).reverse().join(''));
         
